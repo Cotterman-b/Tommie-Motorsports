1f1a93c9984618071f4b4f90e8e105d8b9b049ac
Cotterman-b <bennett.cotterman@gmail.com>
2023-10-26 21:13:20 -0500

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+       
+    extend: {
+      colors: {
+        'text': '#130e01',
+        'background': '#fdf6ff',
+        'primary': '#9531B1',
+        'secondary': '#4B1273',
+        'accent': '#35ff17',
+      },
+      backgroundImage: {
+        'carArches': "url('images/fountain.jpg')"
+      }
+      },
+      backdropBrightness: {
+        35: '.35',
+      },
+      fontFamily: {
+        'mulish': [ 'MULISH', 'sans-serif']
+      },
+  },
+  plugins: [],
+}
+
